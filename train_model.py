6cd20dad7bd17ece72a18185f6f92d6a0b6e3b01
Infernadel <iqbalmuhfirmansyah@gmail.com>
2025-12-07 20:10:04 +0800

    start maybe

--- train_model.py
+++ train_model.py
@@ -143,7 +143,7 @@ numeric_transformer = Pipeline(steps=[
 
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
+    ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False, drop='first'))
 ])
 
 preprocessor = ColumnTransformer(
